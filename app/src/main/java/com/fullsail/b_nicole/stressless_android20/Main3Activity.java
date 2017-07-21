5291a3c95a5e61dbf5c55e1d4cd7a67df7a8bc46
b_nicole <bndarby@fullsail.edu>
2017-07-21 00:03:56 -0400

    Week 3 upload

--- app/src/main/java/com/fullsail/b_nicole/stressless_android20/Main3Activity.java
+++ app/src/main/java/com/fullsail/b_nicole/stressless_android20/Main3Activity.java
@@ -1,12 +1,16 @@
 package com.fullsail.b_nicole.stressless_android20;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,14 +21,20 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Main3Activity extends AppCompatActivity {
 
     private static final String TAG = "Main3Activity";
+    private final int REQ_CODE_SPEECH_OUTPUT = 123;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
+
     TextView emailTextView;
+    Button speakBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +50,48 @@ public class Main3Activity extends AppCompatActivity {
         String profileEmail = mAuth.getCurrentUser().getEmail();
         emailTextView = (TextView) findViewById(R.id.email_profile);
         emailTextView.setText(profileEmail);
+
+        speakBtn = (Button) findViewById(R.id.speak_button);
+
+        speakBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMic();
+            }
+        });
+
+    }
+
+
+    private void openMic(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "What is your request?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_OUTPUT);
+        } catch (ActivityNotFoundException tim){
+
+        }
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case REQ_CODE_SPEECH_OUTPUT:{
+                if(resultCode == RESULT_OK && null != data){
+                    ArrayList<String> voiceIntent = data.getStringArrayListExtra(RecognizerIntent.EXTRA_LANGUAGE);
+                   //this is where I think I make the voice command do something
+
+                }
+                break;
+            }
+        }
 
     }
 
